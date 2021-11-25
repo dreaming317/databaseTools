2583e06e21dfd257b3504a054fb4a6bf004152b4
= <=>
2021-11-26 00:06:46 +0800

    提交MySQL导出word文档工具源代码。
    连接MySQL数据库生成数据库设计文档，导出表的数据库表的设计。

---
+++ src/main/java/com/dream/dbtodoc/utils/UtilHiddleException.java
@@ -0,0 +1,13 @@
+package com.dream.dbtodoc.utils;
+
+/**
+ * Description: 统一异常处理
+ * @author jiujiya
+ * @version 1.0
+ */
+public class UtilHiddleException {
+
+    public static void hiddleException(Throwable e) {
+        e.printStackTrace();
+    }
+}
